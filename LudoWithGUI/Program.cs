15d4c5b1c45b757452ca1d3e6a02b5d46463b280
Minik Gaarde Lambrecht <mini0040@edu.sde.dk>
2018-04-23 11:27:49 +0200

    Add project files.

---
+++ LudoWithGUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LudoWithGUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
